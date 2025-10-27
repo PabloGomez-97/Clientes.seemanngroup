fb1a76edcfee4533cd0ab0de2298f2e01f240179
Pablo <89890109+PabloGomez-97@users.noreply.github.com>
2025-10-27 00:20:02 -0300

    Update Reports.tsx

--- src/components/reports/Reports.tsx
+++ src/components/reports/Reports.tsx
@@ -1060,7 +1060,7 @@ function Reports() {
                     cx="50%"
                     cy="50%"
                     labelLine={false}
-                    label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(0)}%`}
+                    label={(entry: any) => `${entry.name}: ${((entry.percent as number) * 100).toFixed(0)}%`}
                     outerRadius={80}
                     fill="#8884d8"
                     dataKey="value"
