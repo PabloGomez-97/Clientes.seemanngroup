f7cb2bd86e06a70064961e799c3cbf3215d452b0
Pablo <89890109+PabloGomez-97@users.noreply.github.com>
2025-12-01 08:56:28 -0300

    Update Sidebar.tsx

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,5 +1,5 @@
 // src/components/layout/Sidebar.tsx - PREMIUM VERSION
-import { useState } from 'react';
+import { useState, type JSX } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import logoSeemann from './logoseemann.png';
 
