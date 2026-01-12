5979627381224bc3d8eaf4af23381cc76171ef4f
Pablo <89890109+PabloGomez-97@users.noreply.github.com>
2026-01-12 18:21:22 -0300

    Update PiecestypesLCL.tsx

--- src/components/quotes/PackageTypes/PiecestypesLCL.tsx
+++ src/components/quotes/PackageTypes/PiecestypesLCL.tsx
@@ -5,7 +5,16 @@ export interface PackageType {
 }
 
 export const packageTypeOptions: PackageType[] = [
-    { id: 97, name: "BOX" },
-    { id: 57, name: "CARTON" },
-    { id: 110, name: "CARGA GENERAL" }
+    {
+      id: 97, name: "BOX",
+      code: undefined
+    },
+    {
+      id: 57, name: "CARTON",
+      code: undefined
+    },
+    {
+      id: 110, name: "CARGA GENERAL",
+      code: undefined
+    }
 ];
