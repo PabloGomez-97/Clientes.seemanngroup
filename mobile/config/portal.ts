015acacae9686356cf792917ecbc76fb320c96ee
Pablo <89890109+PabloGomez-97@users.noreply.github.com>
2026-07-22 20:27:31 -0400

    Privacidad mobile

--- mobile/config/portal.ts
+++ mobile/config/portal.ts
@@ -1,5 +1,6 @@
 export const PORTAL_BASE_URL = "https://portalclientes.seemanngroup.com";
+export const PUBLIC_SITE_URL = "https://www.seemanngroup.com";
 
-export const PRIVACY_POLICY_URL = `${PORTAL_BASE_URL}/privacy-policy`;
+export const PRIVACY_POLICY_URL = `${PUBLIC_SITE_URL}/privacidad`;
 export const TERMS_OF_SERVICE_URL = `${PORTAL_BASE_URL}/terms-of-service`;
 export const ACCOUNT_DELETION_EMAIL = "privacidad@seemanngroup.com";
