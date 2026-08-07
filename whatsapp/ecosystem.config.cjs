55c0f61f51d831ad2e0ffb294ff09a901bbf0cb5
Pablo <89890109+PabloGomez-97@users.noreply.github.com>
2026-08-07 16:32:19 -0400

    Update ecosystem.config.cjs

--- whatsapp/ecosystem.config.cjs
+++ whatsapp/ecosystem.config.cjs
@@ -1,20 +1,20 @@
-{
-  "apps": [
+module.exports = {
+  apps: [
     {
-      "name": "whatsapp-worker",
-      "cwd": "/opt/seemann-portal",
-      "script": "npm",
-      "args": "run whatsapp:worker",
-      "interpreter": "none",
-      "instances": 1,
-      "autorestart": true,
-      "max_restarts": 20,
-      "min_uptime": "10s",
-      "exp_backoff_restart_delay": 2000,
-      "env": {
-        "NODE_ENV": "production",
-        "LOG_LEVEL": "info"
-      }
-    }
-  ]
-}
+      name: 'whatsapp-worker',
+      cwd: '/opt/seemann-portal',
+      script: 'npm',
+      args: 'run whatsapp:worker',
+      interpreter: 'none',
+      instances: 1,
+      autorestart: true,
+      max_restarts: 20,
+      min_uptime: '10s',
+      exp_backoff_restart_delay: 2000,
+      env: {
+        NODE_ENV: 'production',
+        LOG_LEVEL: 'info',
+      },
+    },
+  ],
+};
